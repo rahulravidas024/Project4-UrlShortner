a7c3830a3476e76f933e68ea3be5abe7b8c32429
rahulravidas024 <rahulravidas024@gmail.com>
2022-10-08 21:22:41 +0530

    project 4 done

---
+++ src/routes/route.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const urlController = require("../controllers/urlController")
+
+
+router.get('/test', function(req, res){
+    return res.send({status: true, msg: "running"})
+})
+
+//-----------URL API---------------
+
+router.post('/url/shorten', urlController. createURL)
+router.get('/:urlCode', urlController.getUrl)
+
+
+module.exports = router
